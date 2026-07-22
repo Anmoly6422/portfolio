1147f36b9363ca882a5edd600c4014a3d05e9004
Anmoly6422 <231046@kit.ac.in>
2026-07-22 12:09:12 +0530

    updating the marquee

--- src/sections/ContactSummary.jsx
+++ src/sections/ContactSummary.jsx
@@ -34,11 +34,13 @@ const ContactSummary = () => {
     });
   }, []);
   return (
-    <section
-      ref={containerRef}
-      className="flex flex-col items-center justify-between min-h-screen gap-12 mt-16"
-    >
-      <Marquee items={items} />
+   <section
+  ref={containerRef}
+  className="flex flex-col items-center justify-center min-h-screen py-20 gap-20"
+>
+     <div className="w-full">
+  <Marquee items={items} />
+</div>
       <div className="overflow-hidden font-light text-center contact-text-responsive">
         <p>
           “ Let’s build a <br />
@@ -47,13 +49,15 @@ const ContactSummary = () => {
           web application <span className="text-gold">together</span> “
         </p>
       </div>
-      <Marquee
-        items={items2}
-        reverse={true}
-        className="text-black bg-transparent border-y-2"
-        iconClassName="stroke-gold stroke-2 text-primary"
-        icon="material-symbols-light:square"
-      />
+      <div className="w-full mt-8">
+  <Marquee
+    items={items2}
+    reverse
+    className="text-black bg-transparent border-y-2"
+    iconClassName="stroke-gold stroke-2 text-primary"
+    icon="material-symbols-light:square"
+  />
+</div>
     </section>
   );
 };
